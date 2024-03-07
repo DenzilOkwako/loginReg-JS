c7b583aedec965a872a8c18c46ed9cb6f876a0a3
DenzilOkwako <denzilwawire418@gmail.com>
2024-03-08 01:09:07 +0300

    #

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
- const formOpenBtn =document.querySelector('#form-open'),
-
- home = document.querySelector('.home'),
- formContainer = document.querySelector('.form_container')
- formCloseBtn = document.querySelector('.form_close'),
+const formOpenBtn = document.querySelector("#form-open")
+ home = document.querySelector(".home"),
+ form_container = document.querySelector(".form_container")
+ formCloseBtn = document.querySelector(".form_close"),
 
  signupBtn = document.querySelector("#signup"),
  loginBtn = document.querySelector("#signup"),
- pwShowHide = document.querySelector(".pw_hide")
+ pwShowHide = document.querySelectorAll(".pw_hide")
 
  formOpenBtn.addEventListener("click",()=> home.classList.add("show"))
  formCloseBtn.addEventListener("click",()=> home.classList.remove("show"))
